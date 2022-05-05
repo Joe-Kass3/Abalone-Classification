2017a4aff959e8556f21b9658850f06dcaaa1c4a
Joe-Kass3 <90224683+Joe-Kass3@users.noreply.github.com>
2022-05-05 12:27:29 -0400

    Add files via upload

--- tree.py
+++ tree.py
@@ -9,14 +9,9 @@ import numpy as np
 import pandas as pd
 from sklearn import model_selection
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+import time
 
-
-# model = DecisionTreeRegressor()
-
-# model.fit(feat_train, tar_train)
-
-# predicted_test = model.predict(feat_test)
-# predicted_train = model.predict(feat_train)
 
 def plot_predicted(real_data, predicted_data):
     """
@@ -54,12 +49,11 @@ def plot_predicted(real_data, predicted_data):
 
 def iter_max_depth(n, m, step_size, feat_train, feat_test, tar_train, tar_test):
     """
-
     Parameters
     ----------
     n : starting number to train max depth
     m : ending number to train max depth
-    feature and target data : data split by train_test_split above
+    feature and target data : data split by train_test_split
 
     Returns
     -------
@@ -86,6 +80,40 @@ def iter_max_depth(n, m, step_size, feat_train, feat_test, tar_train, tar_test):
         
     return(rms_test, rms_train)
 
+def iter_random_forest(n, m, feat_train, feat_test, tar_train, tar_test):
+    """
+    Parameters
+    ----------
+    n : min number of estimators to train
+    m : max number of estimators to train
+    feature and target data : data split by train_test_split
+
+    Returns
+    -------
+    rms_test : error in the testing set
+    rms_train : error in training set
+
+    """
+    rms_test = []
+    rms_train = []
+    
+    for i in range(n, m):
+        model = RandomForestRegressor(n_estimators=i)
+    
+        model.fit(feat_train, tar_train)
+    
+        predicted_test = model.predict(feat_test)
+        predicted_train = model.predict(feat_train)
+
+        rms_test.append(plot_predicted(tar_train, predicted_train))
+        plt.title("Training data -  Max Depth: %s" % i)
+        rms_train.append(plot_predicted(tar_test, predicted_test))
+        plt.title("Test data -  Max Depth: %s" % i)
+        
+        plt.clf() 
+        
+    return(rms_test, rms_train)
+
 def plot_error(rms_test, rms_train):
     """
     Parameters
@@ -98,10 +126,9 @@ def plot_error(rms_test, rms_train):
     plt.plot(rms_test, label='Testing') 
     plt.plot(rms_train, label='Training')
     plt.legend(loc=5)
-   
+    # plt.title("Increasing Size of Random Forest")
+    # # plt.xlabel('Increment of 5 levels')
     plt.ylabel('RMS Error')
-    
-    
     
     
 if __name__ == "__main__":
@@ -114,21 +141,28 @@ if __name__ == "__main__":
     data = pd.get_dummies(data)
 
     target = data['Rings'].values
-    data['Rings'].drop
-
+    data = data.drop(['Rings'],1)
+    
     feature = data.values.astype(float)
+    
 
     feat_train, feat_test, tar_train, tar_test = model_selection.train_test_split(feature, target)
     
-    rms_test, rms_train = iter_max_depth(5, 105, 5, feat_train, feat_test, tar_train, tar_test)
+    # Below confirms that automatic depth of the decision tree is 26 and number of unique features is 28
+    model = DecisionTreeRegressor()
+    model.fit(feat_train, tar_train)
+    print("Depth of Tree: " + str(model.get_depth()))
+    print("Number of leaves: " + str(model.get_n_leaves()))
+    print("Number of possible ages: " + str(len(np.unique(target))))
     
+    # Below investigates importance of max depth of tree
+    # start = time.time()
+    rms_test, rms_train = iter_max_depth(5, 106, 5, feat_train, feat_test, tar_train, tar_test)
+    # end = time.time()
+    # print(end - start)
     plot_error(rms_test, rms_train)
     
-    # print(rms_test)
-    # print(rms_train)
+    # Below investigates importance of using a random forest ensemble
+    rms_test, rms_train = iter_random_forest(10, 50, feat_train, feat_test, tar_train, tar_test)
+    plot_error(rms_test, rms_train)
     
-    # print(1)
-
-
-
-
